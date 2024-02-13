3d7d152693957b8fd98f9499ed3098e06c450e49
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-13 21:42:17 +0500

    add dropdown and textfield

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -4,15 +4,54 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
+
+  var size = ["Small","Medium","Large"];
+  String? dropValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("AI Image Generate"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Search Image",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  value: dropValue,
+                  hint: Text("Select Size"),
+                  items: List.generate(
+                      size.length,
+                        (index) => DropdownMenuItem(
+                          child: Text(size[index]),
+                        ),
+                  ),
+                  onChanged: (value){},
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
