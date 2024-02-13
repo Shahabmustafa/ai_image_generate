d996a81c409e9b4243ef55e504c055e93d6ac857
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-13 21:42:40 +0500

    add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ai_generate_image/view/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AI Image Generate',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomeScreen(),
     );
   }
 }
