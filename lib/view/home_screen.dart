cc5d1dfea9c1e9d8b6ae8f1768d55500d4782ae9
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-14 11:04:17 +0500

    add drop down and generate button

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -19,39 +19,83 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("AI Image Generate"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            Expanded(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Search Image",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton(
-                  value: dropValue,
-                  hint: Text("Select Size"),
-                  items: List.generate(
-                      size.length,
-                        (index) => DropdownMenuItem(
-                          child: Text(size[index]),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5,vertical: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Search Image",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
                         ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 60,
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton(
+                            value: dropValue,
+                            hint: Text("Select Size"),
+                            icon: Icon(Icons.arrow_drop_down),
+                            items: List.generate(
+                              size.length,
+                                  (index) => DropdownMenuItem(
+                                value: size[index],
+                                child: Text(size[index]),
+                              ),
+                            ),
+                            onChanged: (value){},
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  onChanged: (value){},
-                ),
+                  Container(
+                    height: 55,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Generate",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
