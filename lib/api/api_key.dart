10c0ad081f93a77bb0f31d11f0638d5ef7d5d2de
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-14 16:44:36 +0500

    this file i add api keys

--- lib/api/api_key.dart
+++ lib/api/api_key.dart
@@ -1,4 +1,11 @@
 
 class ApiKey{
   static String apiKey = "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";
+
+  static String openAiLinks = "https://api.openai.com/v1/images/generations";
+
+  static final headers = {
+    "Content-Type" : "application/json",
+    "Authorization" : "Bearer $apiKey"
+  };
 }
